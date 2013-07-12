d8b1dc22ab358ed9ee6eaf5a529510e05419e5d8
sagitari <sagitari912@me.com>
2013-07-12 16:38:04 +0200

    Update drawParallelKoor.js

--- exercise_3/drawParallelKoor.js
+++ exercise_3/drawParallelKoor.js
@@ -90,9 +90,9 @@ d3.csv("data/sample_data.csv", function(essen) {
       .on('click', function(d,i){
         // console.log(this);
         d3.select(this).style('stroke-width', '5px');
-        d3.select(this).style('stroke', 'yellow');
+        //d3.select(this).style('stroke', 'yellow');
       })
-      .on('mouseout', function(d,i){
+      .on('click', function(d,i){
         // console.log(this);
         d3.select(this).style('stroke-width', '1.5px');
 
